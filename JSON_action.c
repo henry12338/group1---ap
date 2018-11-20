8d0f1112f3ff01fe85bd4d9c090e38d8ed6b375a
SE-group1 <se-group1@segroup1-VirtualBox.(none)>
2018-11-20 12:10:57 +0800

    add length

--- JSON_action.c
+++ JSON_action.c
@@ -6,83 +6,99 @@
 
 int Request_Controller_Alive(char * OutputBuf)
 {
-	snprintf(OutputBuf, 1000, "{\n  \"Action\" : 0\n}\n");
+        const int length = 1000;
+	snprintf(OutputBuf, length, "{\n  \"Action\" : 0\n}\n");
 	return 1;
 }
 int Request_Register_AP(const char * ip, char * OutputBuf)
 {
-        snprintf(OutputBuf, 1000, "{\n  \"Action\" : 1,\n  \"AP out IP\" : \"%s\"\n}\n", ip);
+        const int lenght = 1000;
+        snprintf(OutputBuf, length, "{\n  \"Action\" : 1,\n  \"AP out IP\" : \"%s\"\n}\n", ip);
 	return 1;
     //neccessary register info
 }
 int Request_Return_AP_Info(uint32_t AP_Number, char * OutputBuf)
 {
-        snprintf(OutputBuf, 1000, "{\n  \"Action\" : 2,\n  \"AP Number\" : %u\n}\n", AP_Number);
+        const int length = 1000;
+        snprintf(OutputBuf, length, "{\n  \"Action\" : 2,\n  \"AP Number\" : %u\n}\n", AP_Number);
 	return 1;
 }
 int Request_User_IsRegistered(const char * Username, char * OutputBuf)
 {
-        snprintf(OutputBuf, 1000, "{\n  \"Action\" : 3,\n  \"Username\" : \"%s\"\n}\n", Username);
+        const int length = 1000;
+        snprintf(OutputBuf, length, "{\n  \"Action\" : 3,\n  \"Username\" : \"%s\"\n}\n", Username);
 	return 1;
 }
 int Request_Check_User_Password(const char * Hashed_Password, char * OutputBuf)
 {
-        snprintf(OutputBuf, 1000, "{\n  \"Action\" : 4,\n  \"Password\" : \"%s\"\n}\n", Hashed_Password);
+        const int length = 1000;
+        snprintf(OutputBuf, length, "{\n  \"Action\" : 4,\n  \"Password\" : \"%s\"\n}\n", Hashed_Password);
 	return 1;
 }
 int Request_Send_User_Info(uint32_t AP_Number, char * OutputBuf)
 {
-        snprintf(OutputBuf, 1000, "{\n  \"Action\" : 5,\n  \"AP Number\" : %u\n}\n", AP_Number);
+        const int length = 1000;
+        snprintf(OutputBuf, length, "{\n  \"Action\" : 5,\n  \"AP Number\" : %u\n}\n", AP_Number);
 	return 1;
 }
 int Request_Upload_Config(uint32_t AP_Number, const char * ConfigLocation, char * OutputBuf)
 {
-        snprintf(OutputBuf, 1000, "{\n  \"Action\" : 6,\n  \"AP Number\" : \"%u\",\n  \"Config Archive Location\" : \"%s\"\n}\n", AP_Number, ConfigLocation);
+        const int length = 1000;
+        snprintf(OutputBuf, length, "{\n  \"Action\" : 6,\n  \"AP Number\" : \"%u\",\n  \"Config Archive Location\" : \"%s\"\n}\n", AP_Number, ConfigLocation);
 	return 1;
 }
 int Request_Download_Config(const char * ConfigLocation, char * OutputBuf)
 {
-        snprintf(OutputBuf, 1000, "{\n  \"Action\" : 7,\n  \"Config Archive Location\" : \"%s\"\n}\n", ConfigLocation);
+        const int length = 1000;
+        snprintf(OutputBuf, length, "{\n  \"Action\" : 7,\n  \"Config Archive Location\" : \"%s\"\n}\n", ConfigLocation);
 	return 1;
 }
 //---------Response------------
 int Response_Controller_Alive(char * OutputBuf)
 {
-    snprintf(OutputBuf, 1000, "{\n  \"Action\" : 0,\n  \"State\" : 1\n}\n");
+    const int length = 1000;
+    snprintf(OutputBuf, length, "{\n  \"Action\" : 0,\n  \"State\" : 1\n}\n");
     return 1;
 }
 int Response_Register_AP(uint32_t AP_Number, char * OutputBuf)
 {
-    snprintf(OutputBuf, 1000, "{\n  \"Action\" : 1,\n  \"AP Number\" : %u\n}\n", AP_Number);
+    const int length = 1000;
+    snprintf(OutputBuf, length, "{\n  \"Action\" : 1,\n  \"AP Number\" : %u\n}\n", AP_Number);
     return 1;
 }
 int Response_Return_AP_Info(uint32_t AP_Number, char * OutputBuf, uint32_t AP_Temperature, double AP_Usage_Percent, uint32_t AP_Device_Amount, uint32_t AP_Traffic)
 {
-    snprintf(OutputBuf, 1000, "{\n  \"Action\" : 2,\n  \"AP Number\" : %u,\n  \"AP CPU temperture\" : %u,\n  \"AP CPU Usage Percent\" : %.2f,\n  \"Connect Device Amount\" : %u,\n  \"Traffic(Kib)\" : %u\n}\n", AP_Number, AP_Temperature, AP_Usage_Percent, AP_Device_Amount, AP_Traffic);
+    const int length = 1000;
+    snprintf(OutputBuf, length, "{\n  \"Action\" : 2,\n  \"AP Number\" : %u,\n  \"AP CPU temperture\" : %u,\n  \"AP CPU Usage Percent\" : %.2f,\n  \"Connect Device Amount\" : %u,\n  \"Traffic(Kib)\" : %u\n}\n", AP_Number, AP_Temperature, AP_Usage_Percent, AP_Device_Amount, AP_Traffic);
     return 1;
 }
 int Response_User_IsRegistered(int IsRegistered, char * OutputBuf)
 {
-    snprintf(OutputBuf, 1000, "{\n  \"Action\" : 3,\n  \"Result\" :%d\n}\n", IsRegistered);
+    const int length = 1000;
+    snprintf(OutputBuf, length, "{\n  \"Action\" : 3,\n  \"Result\" :%d\n}\n", IsRegistered);
     return 1;
 }
 int Response_Check_User_Password(int CheckPassword, char * OutputBuf)
 {
-    snprintf(OutputBuf, 1000, "{\n  \"Action\" : 4,\n  \"Result\" : %d\n}\n", CheckPassword);
+    const int length = 1000;
+    snprintf(OutputBuf, length, "{\n  \"Action\" : 4,\n  \"Result\" : %d\n}\n", CheckPassword);
     return 1;
 }
 int Response_Send_User_Info(const char * ip, const char *Username, char * OutputBuf)
 {
-    snprintf(OutputBuf, 1000, "{\n  \"Action\" : 5,\n  \"User Out IP\" : \"%s\",\n  \"Username\" : \"%s\"\n}\n", ip, Username);
+    const int length = 1000;
+    snprintf(OutputBuf, length, "{\n  \"Action\" : 5,\n  \"User Out IP\" : \"%s\",\n  \"Username\" : \"%s\"\n}\n", ip, Username);
     return 1;
 }
 int Response_Upload_Config(uint32_t AP_Number, const char * ConfigLocation, char * OutputBuf)
 {
-    snprintf(OutputBuf, 1000, "{\n  \"Action\" : 6,\n  \"AP Number\" : %u,\n  \"Config Archive Location\" : \"%s\"\n}\n", AP_Number, ConfigLocation);
+    const int length = 1000;
+    snprintf(OutputBuf, length, "{\n  \"Action\" : 6,\n  \"AP Number\" : %u,\n  \"Config Archive Location\" : \"%s\"\n}\n", AP_Number, ConfigLocation);
     return 1;
 }
 int Response_Download_Config(const char * ConfigLocation, char * OutputBuf)
 {
-    snprintf(OutputBuf, 1000, "{\n  \"Action\" : 7,\n  \"Config Archive Location\" : \"%s\"\n}\n", ConfigLocation);
+    const int length = 1000;
+    snprintf(OutputBuf, length, "{\n  \"Action\" : 7,\n  \"Config Archive Location\" : \"%s\"\n}\n", ConfigLocation);
     return 1;
 }
--- JSON_request_handle.c
+++ JSON_request_handle.c
@@ -44,7 +44,8 @@ int Handle_Return_AP_Info(const char * InputBuf, int socket_fd)
 
 
 	char OutputBuf[1000];
-        snprintf(OutputBuf, 1000, "{\n  \"Action\" :2,\n  \"AP Number\" : %u,\n  \"AP CPUT temperature(Celcius)\" : %u,\n  \"AP CPU Usage Percent\" :%.2f,\n  \"Connect Device Amount\" :%u,\n  \"Network Traffic(KiB)\" : %u\n}\n", (uint32_t)AP_Number, AP_Temperature, AP_Usage_Percent, AP_Device_Amount, AP_Traffic);
+        const int length = 1000;
+        snprintf(OutputBuf, length, "{\n  \"Action\" :2,\n  \"AP Number\" : %u,\n  \"AP CPUT temperature(Celcius)\" : %u,\n  \"AP CPU Usage Percent\" :%.2f,\n  \"Connect Device Amount\" :%u,\n  \"Network Traffic(KiB)\" : %u\n}\n", (uint32_t)AP_Number, AP_Temperature, AP_Usage_Percent, AP_Device_Amount, AP_Traffic);
 	return send(socket_fd, OutputBuf, strlen(OutputBuf)+1, 0);
 }
 int Handle_Send_Connected_User_Number(const char * InputBuf, int socket_fd)
@@ -58,7 +59,8 @@ int Handle_Send_Connected_User_Number(const char * InputBuf, int socket_fd)
 
 
 	char OutputBuf[1000];
-        snprintf(OutputBuf, 1000, "{\n  \"Action\" : 5,\n  \"AP Number\" : %u,\n  \"Connected User Number\" : %u\n}\n", (uint32_t)AP_Number,Connected_User_Number);
+        const int length = 1000;
+        snprintf(OutputBuf, length, "{\n  \"Action\" : 5,\n  \"AP Number\" : %u,\n  \"Connected User Number\" : %u\n}\n", (uint32_t)AP_Number,Connected_User_Number);
 	return send(socket_fd, OutputBuf, strlen(OutputBuf)+1, 0);
 
 }
@@ -72,7 +74,8 @@ int Handle_Send_User_Info(const char * InputBuf, int socket_fd)
 	printf("Username is %s\n", Username);
 
 	char OutputBuf[1000];
-        snprintf(OutputBuf, 1000, "{\n  \"Action\" : 6,\n  \"AP Number\" : %u,\n  \"User Out IP\" : %s,\n  \"Username\" : %s\n}\n", (uint32_t)AP_Number, User_External_IP, Username);
+        const int length = 1000;
+        snprintf(OutputBuf, length, "{\n  \"Action\" : 6,\n  \"AP Number\" : %u,\n  \"User Out IP\" : %s,\n  \"Username\" : %s\n}\n", (uint32_t)AP_Number, User_External_IP, Username);
 	return send(socket_fd, OutputBuf, strlen(OutputBuf)+1, 0);
 }
 int Handle_Download_Config(const char * InputBuf, int socket_fd)
@@ -81,6 +84,7 @@ int Handle_Download_Config(const char * InputBuf, int socket_fd)
 	printf("Config Archive Location is in %s\n", Config_Location);
 
 	char OutputBuf[1000];
-        snprintf(OutputBuf, 1000, "{\n  \"Action\" : 8,\n  \"Config Archive Location\" : %s\n}\n", Config_Location);
+        const int length = 1000;
+        snprintf(OutputBuf, length, "{\n  \"Action\" : 8,\n  \"Config Archive Location\" : %s\n}\n", Config_Location);
 	return send(socket_fd, OutputBuf, strlen(OutputBuf)+1, 0);
 }
